c495ad300cce4d4b5eda3a0bbdb3678aa262b35b
FabioCosentUni <fabio.cosentino001@studenti.uniparthenope.it>
2023-12-12 11:12:40 +0100

    Create login servlet mapping loginServlet delete mapping utenteServlet

---
+++ src/main/java/controller/LoginServlet.java
@@ -0,0 +1,50 @@
+package controller;
+
+import exception.user.UserException;
+import model.Utente;
+import service.UtenteService;
+import service.impl.UtenteServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    private UtenteService utenteService;
+    public void init() {
+        try{
+            super.init();
+            utenteService = new UtenteServiceImpl();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getPathInfo();
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        Utente u = null;
+        try {
+            u = utenteService.login(email, password);
+            request.getSession().setAttribute("utente", u);
+            request.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+        } catch (UserException e) {
+            request.setAttribute("error", e.getErrorCause());
+            request.setAttribute("email", request.getParameter("email"));
+            request.setAttribute("password", request.getParameter("password"));
+            request.getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(500, e.getMessage());
+        }
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+}
